b142cacbdd9f7d2625c775bf7a8e694c88ab3938
AdityaSharma151 <adityasharmadps1124@gmail.com>
2025-03-28 15:36:06 +0530

    signin Route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -29,7 +29,25 @@ export async function signup(req, res) {
 }
 
 export async function login(req, res) {
-    res.send('Login');
+   try {
+     const {username, password} = req.body;
+     if (!username || !password) {
+       return res.status(400).json({ message: 'Username and password are required' });
+     }
+     const user = await User.findOne({ username });
+     if (!user) {
+       return res.status(400).json({ message: 'Invalid username or password' });
+     }
+     const isValidPassword = await bcrypt.compare(password, user.password);
+     if (!isValidPassword) {
+       return res.status(400).json({ message: 'Invalid username or password' });
+     }
+     res.status(200).json({ message: 'Logged in successfully' });
+ }
+    catch (error) {
+      res.status(500).json({ message: error.message });
+    
+   }
 }
 
 export async function logout(req, res) {
