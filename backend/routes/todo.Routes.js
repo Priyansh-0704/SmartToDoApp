81255f3a29dd6b61e5556d6b2e4d2a6ce6eef691
Priyansh 0704 <priyanshagarwal0704@gmail.com>
2025-03-30 16:14:24 +0530

    added todo controller and routes

---
+++ backend/routes/todo.Routes.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import { createTodo } from '../controllers/todo.controller.js';
+
+const router = express.Router();
+router.post('/create', createTodo);
+
+export default router;
