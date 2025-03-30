4c8fc8c42050d300863a822768bb053dc440e0cf
AdityaSharma151 <adityasharmadps1124@gmail.com>
2025-03-30 19:07:13 +0530

    fixed the create route

--- backend/controllers/todo.controller.js
+++ backend/controllers/todo.controller.js
@@ -1,20 +1,20 @@
-import Todo from "../models/todo.models.js";
 import User from "../models/user.models.js";
+import Todo from "../models/todo.models.js";
+
 
 export async function createTodo(req, res) {
   try {
     const { title, description, completed, reminder, priority, username } = req.body;
-
+    
     if (!title || !description) {
       return res.status(400).json({ message: "Title and description are required" });
     }
-
+    
     const user = await User.findOne({ username });
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
-
-    // Create a new Todo
+    
     const todo = new Todo({
       title,
       description,
@@ -23,15 +23,13 @@ export async function createTodo(req, res) {
       priority: priority || "medium",
       user: user._id,
     });
-
+    
     const savedTodo = await todo.save();
-
     
     user.list.push(savedTodo);
     await user.save();
-
+    
     res.status(201).json({ message: "Todo created successfully", todo: savedTodo });
-
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
