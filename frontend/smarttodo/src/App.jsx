e33b38ed7f47419cb6de358aebee15754ccdd9f7
Priyansh 0704 <priyanshagarwal0704@gmail.com>
2025-04-08 12:19:44 +0530

    added a navigation path between home and about

--- frontend/smarttodo/src/App.jsx
+++ frontend/smarttodo/src/App.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./components/home/Home";
 import Footer from "./components/footer/Footer"
+import About from "./components/about/About";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-      <Navbar />
-      <Home />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path = "/" element = {<Home />} />
+          <Route path = "/about" element = {<About />} />
+          <Route path = "/signup" element = {<About />} />
+          <Route path = "/signin" element = {<About />} />
+        </Routes>
+      </Router>
+
       <Footer />
     </div>
   );
--- frontend/smarttodo/src/components/navbar/Navbar.jsx
+++ frontend/smarttodo/src/components/navbar/Navbar.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import "./navbar.css";
+import { RiContactsBook2Fill} from "react-icons/ri";
+import { Link } from "react-router-dom";
 import { GiWhiteBook } from "react-icons/gi";
 
 const Navbar = () => {
     return (
         <div><nav className="navbar navbar-expand-lg">
             <div className="container">
-                <a className="navbar-brand" href="#">
+                <Link className="navbar-brand" to="/">
                     <b><GiWhiteBook /> &nbsp; todo</b>
-                </a>
+                </Link>
 
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -16,27 +18,27 @@ const Navbar = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li className="nav-item mx-2">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                         </li>
 
                         <li className="nav-item mx-2">
-                            <a className="nav-link active" aria-current="page" href="#">About Us</a>
+                            <Link className="nav-link active" aria-current="page" to="/about">About Us</Link>
                         </li>
 
                         <li className="nav-item mx-2">
-                            <a className="nav-link active" aria-current="page" href="#">Todo</a>
+                            <Link className="nav-link active" aria-current="page" to="/todo">Todo</Link>
                         </li>
 
                         <li className="nav-item mx-2">
-                            <a className="nav-link active btn-nav" aria-current="page" href="#">Sign Up</a>
+                            <Link className="nav-link active btn-nav" aria-current="page" to="/signup">Sign Up</Link>
                         </li>
 
                         <li className="nav-item mx-2">
-                            <a className="nav-link active btn-nav" aria-current="page" href="#">Sign In</a>
+                            <Link className="nav-link active btn-nav" aria-current="page" to="/signin">Sign In</Link>
                         </li>
 
                         <li className="nav-item mx-2">
-                            <a className="nav-link active btn-nav" aria-current="page" href="#">Log Out</a>
+                            <Link className="nav-link active btn-nav" aria-current="page" to="#">Log Out</Link>
                         </li>
                     </ul>
                 </div>
