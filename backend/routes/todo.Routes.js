e02e2fcf4d1b444baff6885c81582233a0b709e3
Aditya Sharma <141180239+AdityaSharma151@users.noreply.github.com>
2025-04-14 15:13:29 +0530

    added new routes

--- backend/routes/todo.Routes.js
+++ backend/routes/todo.Routes.js
@@ -1,10 +1,15 @@
 import express from 'express';
-import { createTodo, getTask, updateTodo, deleteTodo } from '../controllers/todo.controller.js';
+import { createTodo } from '../controllers/todo.controller.js';
+import { updateTodo } from '../controllers/todo.controller.js';
+import { deleteTodo } from '../controllers/todo.controller.js';
+import { getTask } from '../controllers/todo.controller.js';
+import {toggleComplete} from '../controllers/todo.controller.js';
 
 const router = express.Router();
 router.post('/create', createTodo);
 router.put('/update/:id', updateTodo);
 router.delete('/delete/:id', deleteTodo);
 router.get('/getTask/:id', getTask);
+router.patch('/toggleCompletion/:id', toggleComplete);
 
-export default router;
+export default router;
